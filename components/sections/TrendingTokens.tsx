f9ea3b9784e4f63ca49a3680d08f87d4327a83e0
Luke Carter <everythingsimpleinc1@gmail.com>
2025-08-21 21:18:01 +0100

    Fix gradient fade edges to be consistently vertical

--- components/sections/TrendingTokens.tsx
+++ components/sections/TrendingTokens.tsx
@@ -176,8 +176,8 @@ const TrendingTokens: React.FC = () => {
           )}
           
           {/* Gradient Fade Edges */}
-          <div className="absolute left-0 top-0 bottom-4 w-8 bg-gradient-to-r from-background-dark to-transparent pointer-events-none"></div>
-          <div className="absolute right-0 top-0 bottom-4 w-8 bg-gradient-to-l from-background-dark to-transparent pointer-events-none"></div>
+          <div className="absolute left-0 top-0 bottom-0 w-8 bg-gradient-to-r from-background-dark to-transparent pointer-events-none z-10"></div>
+          <div className="absolute right-0 top-0 bottom-0 w-8 bg-gradient-to-l from-background-dark to-transparent pointer-events-none z-10"></div>
         </div>
       </div>
     </section>
